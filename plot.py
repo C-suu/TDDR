7ab6a18429b37858f4329fb09116ace799cf12fc
H Chen <153593101+C-suu@users.noreply.github.com>
2026-02-14 22:55:47 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,4 +1,4 @@
-# 这是TDDR论文的图2的子图b，是HalfCheetah运行五次的画图程序。其他环境和其他算法同理，只需要运行不同算法和不同环境，即可画出TDDR的图2，3，4。
+# This script reproduces Figure 2 (b) from the TDDR paper, visualizing the performance in HalfCheetah across five random seeds. This pipeline is generally applicable to other baselines and environments: simply execute the respective algorithms across the desired environments to reproduce Figures 2, 3, and 4 from the paper.
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -17,11 +17,10 @@ for environment in environments:
 
     all_data = {'TDDR': []}
 
-    # 文件夹路径
-    # 将这个路径替换为数据文件所在的路径，其余不需要修改
-    folder_path = r'C:\Users\123\Desktop\新建文件夹\HalfCheetah'
+    # Directory path
+    # Update this path to your local data directory; no further modifications are required.
+    folder_path = r'.\HalfCheetah'
 
-    # 平滑处理函数
     def smooth_curve(data, smoothing_window=n_smooth):
         smoothed_data = []
         for i in range(len(data)):
@@ -32,7 +31,7 @@ for environment in environments:
 
     genshin_impact_colors = [(208, 119, 151)]
        
-    # 将 RGB 值转换为 [0, 1] 范围内的值
+    # Normalize RGB values to the [0, 1] range
     genshin_impact_colors = [(r/255., g/255., b/255.) for r, g, b in genshin_impact_colors]
 
     sns.set_theme(style="darkgrid")
@@ -40,30 +39,30 @@ for environment in environments:
 
     plt.figure(figsize=(10, 6))
 
-    # 读取 5 个种子的数据
+    # Load empirical data across the 5 independent seeds
     for j in range(1, n_range + 1): 
         file_path = os.path.join(folder_path, f'TDDR_{environment}-v2_{j}_avg_rewards.npy')
-        print(f"正在读取: {file_path}")
+        print(f"Loading: {file_path}")
         data = np.load(file_path)
         all_data['TDDR'].append(data)
 
-    # 计算平均值和标准差
+    # Compute the mean and standard deviation
     data_array = np.array(all_data['TDDR'])
     avg_reward = np.mean(data_array, axis=0)
     std_dev = np.std(data_array, axis=0)
 
-    # 平滑处理
+    # Apply curve smoothing
     smoothed_reward = smooth_curve(avg_reward, smoothing_window=n_smooth)
     smoothed_std_dev = smooth_curve(std_dev, smoothing_window=n_smooth)
 
     smoothed_reward = np.array(smoothed_reward)
     smoothed_std_dev = np.array(smoothed_std_dev)
 
-    # 绘制阴影区域
+    # Plot the shaded region
     plt.fill_between(range(len(smoothed_reward)), smoothed_reward - smoothed_std_dev,
                     smoothed_reward + smoothed_std_dev, alpha=0.15)
     
-    # 绘制曲线
+    # Plot the learning curve
     plt.plot(smoothed_reward, label='TDDR', linewidth=2)
 
     plt.xlabel('Million Steps', fontsize='xx-large')
@@ -76,3 +75,4 @@ for environment in environments:
 plt.tight_layout()
 
 plt.show()
+
